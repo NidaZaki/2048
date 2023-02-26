765a917ba54bb696bce783e9fba664aecc987840
NidaZaki <nidazaki97@gmail.com>
2023-02-25 22:51:18 -0500

    css stylin

--- 2048.js
+++ 2048.js
@@ -1,18 +1,19 @@
-let board = [
-    [2, 2, 2, 2],
-    [0, 2, 2, 2],
-    [4, 4, 8, 8],
-    [4, 4, 8, 8]
-  ]
+// let board = [
+//     [2, 2, 2, 2],
+//     [2, 2, 2, 2],
+//     [4, 4, 8, 8],
+//     [4, 4, 8, 8]
+//   ]
 
-  //let boardAfterAddingZeros = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-  var indexRow;
-  var indexCol;
-  var positions = [];
-  
+let board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]  
 
 window.onload = function() {
-    setGame();
+    setGame();  
 }
 
 function setGame(){
@@ -25,6 +26,8 @@ function setGame(){
             document.getElementById("board").append(tile);
         }
     }
+    addInitialNumber();
+    addInitialNumber();
 }  
 
 function updateTile(tile, num){   // add styling for each tile
@@ -46,20 +49,23 @@ document.addEventListener("keydown", (e) => {
     var name = e.code;
     if(name === "ArrowLeft"){
         swipeLeft();
+        addInitialNumber();
     }
     else if( name === "ArrowRight"){
         swipeRight();
+        addInitialNumber();
     }
     else if (name === "ArrowUp"){
         swipeUp();
+        addInitialNumber();
     }
     else if(name === "ArrowDown"){
         swipeDown();
+        addInitialNumber();
     }
 } )
-  //addInitialNumber();
- 
-  function swipeLeft(){
+
+function swipeLeft(){
     addSimilarNumbersForLeft(); 
     addTrailingZeroesForLeft();
     for(let r = 0; r < 4; r++){
@@ -71,7 +77,7 @@ document.addEventListener("keydown", (e) => {
     }
 }
 
-  function swipeRight(){
+function swipeRight(){
     addSimilarNumbersForRight();
     addTrailingZeroesForRight();
     for(let r = 0; r < 4; r++){
@@ -83,28 +89,21 @@ document.addEventListener("keydown", (e) => {
     }
 }
 
-  function addInitialNumber(){
-    for(var r = 0; r < 4; r++){
-        indexRow = parseInt(Math.random() * 4)
-        for(var c = 0; c < 4; c++){
-            indexCol = parseInt(Math.random() * 4)   
-        }
-        if(positions.length < 2){                   // randomly assigning 2's or 4's at any two positions
-            board[indexRow][indexCol] = (Math.random() < 0.5) ? 2 : 4; 
-            positions.push({r : indexRow, c : indexCol});
-        }
-     }
-     console.log(positions)
-  }
+function setTwoOrFour(){
+    return ((Math.random() < 0.5) ? 2 : 4);
+}
 
-// swipe left [2,0,0,2] => [2,2]
-//1. clear all zeroes [2,2,2]
-//2. 0-0, 0-1 gets added to 4 if same numbers, then 4 compared with 2, 
-            //  i. if not same [4,0,2]
-          
-//i. 3. removes zeroes [4,2]
-//4. add zeroes at the end [4,2,0,0] 
-  // ii. if same, repeat 2 [4,4] (same)
+function addInitialNumber(){
+    
+        var r = Math.floor(Math.random() * 4)
+        var c = Math.floor(Math.random() * 4);
+        if(board[r][c] === 0){
+            board[r][c] = setTwoOrFour(); 
+            let tile = document.getElementById(r.toString()+"-"+c.toString());
+            let num = board[r][c];
+            updateTile(tile, num); 
+        }
+}
  
 function clearAllZeroes(){
     for(var r = 0; r < 4; r++){
@@ -132,36 +131,6 @@ function addSimilarNumbersForRight(){
     add01Numbers();
     clearAllZeroes()
 }
-
-// function add03Numbers(){
-//     console.log(board)
-//     for(var r = 0; r < board.length; r++){
-//         for(var c = 0; c < board[r].length; c++){
-//         if((board[r][0] && board[r][3] !== null) && (board[r][1] === 0) && (board[r][2] === 0)){
-          
-//            if(board[r][0] === board[r][3]){             // adding similar numbers
-//             board[r][0] = board[r][0] + board[r][3]
-//             board[r][3] = 0
-//            }
-//         }
-//         }
-//     }
-// }
-
-// function add02Numbers(){
-//     console.log(board)
-//     for(var r = 0; r < board.length; r++){
-//         for(var c = 0; c < board[r].length; c++){
-//         if((board[r][0] && board[r][2] !== null) && (board[r][1] === 0)){
-          
-//            if(board[r][0] === board[r][2]){             // adding similar numbers
-//             board[r][0] = board[r][0] + board[r][2]
-//             board[r][2] = 0
-//            }
-//         }
-//         }
-//     }
-// }
 
 function add01Numbers(){
     for(var r = 0; r < board.length; r++){
@@ -213,7 +182,6 @@ function addTrailingZeroesForLeft(){
             } 
         }
     }
-    console.log(board)
 }
 
 function addTrailingZeroesForRight(){
@@ -224,19 +192,7 @@ function addTrailingZeroesForRight(){
             } 
         }
     }
-    console.log(board)
 }
-
-// UP:
-// [2, 0, 2, 0] -> [4,0]              [4,16] [4,8,0,0]
-// [2, 0, 2, 0] -> [0,0]              [8,0]
-// [4, 8, 4, 0] -> [8,16]              [0,0]
-// [4, 8, 4, 0] -> [0,0,]              [0,0]
-
-// 1. if num > 0, compare == r1 with r2, r1 = sum, r2 = 0
-// 2. r1 = r2 ; r2 = r3
-// 3. clear all zeroes
-// 4. add zeros 
 
 function transposeColsInRows(){
     for(var r = 0 ; r < 4; r++){
@@ -246,7 +202,6 @@ function transposeColsInRows(){
             board[c][r] = initial
         }
     }
-    console.log(board)
 }
 
 function finalTranspose(){
@@ -257,51 +212,7 @@ function finalTranspose(){
             board[c][r] = initial
         }
     }
-    console.log(board)
 }
-
-// function add01ColNumbers(){
-//     for(var r = 0; r < 4; r++){
-//         for (var c = 0; c < 4; c++){
-//             if(board[0][c] && board[0][c] !== null){
-//                 if(board[0][c] === board[1][c]){
-//                     board[0][c] = board[0][c] + board[1][c]
-//                     board[1][c] = 0
-//                 }
-//             }
-            
-//         }
-//     }
-// }
-
-// function add12ColNumbers(){
-//     for(var r = 0; r < 4; r++){
-//         for (var c = 0; c < 4; c++){
-//             if(board[1][c] && board[2][c] !== null){
-//                 if(board[1][c] === board[2][c]){
-//                     board[1][c] = board[1][c] + board[2][c]
-//                     board[2][c] = 0
-//                 }
-//             }
-            
-//         }
-//     }
-//     console.log(board);
-// }
-
-// function add23ColNumbers(){
-//     for(var r = 0; r < 4; r++){
-//         for (var c = 0; c < 4; c++){
-//             if(board[2][c] && board[3][c] !== null){
-//                 if(board[2][c] === board[3][c]){
-//                     board[2][c] = board[2][c] + board[3][c]
-//                     board[3][c] = 0
-//                 }
-//             }  
-//         }
-//     }
-//     console.log(board)
-// }
 
 function addSimilarNumbersForUp(){
     transposeColsInRows();
@@ -311,8 +222,6 @@ function addSimilarNumbersForUp(){
     add23Numbers();
     clearAllZeroes();
 }
-
-//let boardCopy = JSON.parse(JSON.stringify(board));
 
 function swipeUp(){
         addSimilarNumbersForUp();
@@ -340,7 +249,6 @@ function swipeDown(){
             updateTile(tile, num);
         }
     }
-
 }
 
 function addSimilarNumbersForDown(){
